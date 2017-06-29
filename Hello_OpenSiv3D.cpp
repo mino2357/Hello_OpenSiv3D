1fda7c33d49dffab08988442e4b9dabc89954b60
mino2357 <mino2357@gmail.com>
2017-06-29 17:37:26 +0900

    add

--- Hello_OpenSiv3D.cpp
+++ Hello_OpenSiv3D.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <limits>
 #include <iomanip>
+#include <cmath>
 
 namespace mino2357{
     template<typename T = double>
@@ -22,6 +23,8 @@ namespace mino2357{
         inline vector& operator=( const vector<T>&) noexcept;
         inline vector& operator+=(const vector<T>&) noexcept;
         inline vector& operator-=(const vector<T>&) noexcept;
+        
+        inline T norm() noexcept;
         
         inline void printPosition() noexcept;
     };
@@ -74,6 +77,11 @@ namespace mino2357{
     }
     
     template <typename T>
+    inline T vector<T>::norm() noexcept {
+        return std::sqrt(componentX * componentX + componentY * componentY);
+    }
+    
+    template <typename T>
     inline vector<T> operator+(const vector<T>& a, const vector<T>& b) noexcept {
         return vector<T>{
             a.getComponentX() + b.getComponentX(),
@@ -99,26 +107,42 @@ namespace mino2357{
         
         
 void Main(){
+    Graphics::SetTargetFrameRateHz(120);
     Graphics::SetBackground(ColorF(0.8, 0.9, 1.0));
     
     mino2357::vector<> vec1(1., 2.);
     mino2357::vector<> vec2(3., 4.);
     (vec1 + vec2).printPosition();
-    //Log << test.getComponentX();
-    /*
+    
     const Font font(50);
-             
-    const Texture textureCat(Emoji(L"🐈"), TextureDesc::Mipped);
+    //const Texture textureCat(Emoji(L"🐈"), TextureDesc::Mipped);
+    
+    /*
+    Print << L"Hello, Siv3D!";
+    Print << L"1 + 1 = " << (1 + 1);
+    Print << Vec2(12.3, 45.6);
+    Print << Rect(100, 200, 500, 300);
+    Print << DateTime::Now();
+    Print << Range(1, 10);
     */
+    
+    double t {};
+    double r = 100;
+    
     while (System::Update()){
-        Circle(Cursor::Pos(), 40).draw();
+        ClearPrint();
+        
+        Print << Cursor::Pos();
+        
+        t += System::DeltaTime();
+        
+        font(L"Hello OpenSiv3D!.").drawAt(Window::Center(), Palette::Black);
+        Circle(Window::Width()/2 + r * cos(5*t), Window::Height()/2 - r * sin(6*t), 10).draw(Palette::Red);
+        
         /*
         font(L"Hello, Siv3D!🐣").drawAt(Window::Center(), Palette::Black);
-                 
         font(Cursor::Pos()).draw(20, 400, ColorF(0.6));
-                 
-         textureCat.resize(80).draw(540, 380);
-                 
+        textureCat.resize(80).draw(540, 380);
         Circle(Cursor::Pos(), 60).draw(ColorF(1, 0, 0, 0.5));
         */
     }
