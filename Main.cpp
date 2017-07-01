bf2fa48fba9fe947d3f0688dba80d1fde9b5781d
mino2357 <mino2357@gmail.com>
2017-07-02 00:25:17 +0900

    頭が働かない中，あまりにもテキトーに書いてしまった．

--- Main.cpp
+++ Main.cpp
@@ -3,46 +3,64 @@
 #include <limits>
 #include <iomanip>
 #include <cmath>
-#include "myvector.hpp"
+#include <vector>
+
+//今は使わない
+//#include "myvector.hpp"
+
+constexpr int64 N = 128;
+constexpr double L = 3.1415;
+constexpr double dt = 0.005;
 
 void Main(){
     Graphics::SetTargetFrameRateHz(120);
     Graphics::SetBackground(ColorF(0.8, 0.9, 1.0));
     
-    mino2357::vector<> vec1(1., 2.);
-    mino2357::vector<> vec2(3., 4.);
-    (vec1 + vec2).printPosition();
-    
+    std::vector<double> u1(N);
+    std::vector<double> u2(N);
+    std::vector<double> u3(N);
+
+    //for(auto&& x: u1) x = 0.;
+    //for(auto&& x: u2) x = 0.;
+    //for(auto&& x: u3) x = 0.;
+
+    for(uint64 i=0; i<N; ++i){
+        u1[i] = std::exp( - 20 * std::pow(L / (N - 1) * i - L/2, 2));
+        u2[i] = std::exp( - 20 * std::pow(L / (N - 1) * i - L/2, 2));
+        u3[i] = std::exp( - 20 * std::pow(L / (N - 1) * i - L/2, 2));
+    }
+
+    double width  = Window::Width();
+
+    double dWidth = width / (N - 1);
+
     const Font font(50);
-    //const Texture textureCat(Emoji(L"🐈"), TextureDesc::Mipped);
-    
-    /*
-    Print << L"Hello, Siv3D!";
-    Print << L"1 + 1 = " << (1 + 1);
-    Print << Vec2(12.3, 45.6);
-    Print << Rect(100, 200, 500, 300);
-    Print << DateTime::Now();
-    Print << Range(1, 10);
-    */
     
     double t {};
-    double r = 100;
-    
+    //double r = 100;
+    double dx = L / (N - 1);
+    double lam = (dt * dt)/(dx * dx);
+
     while (System::Update()){
         ClearPrint();
-        
         Print << Cursor::Pos();
-        
         t += System::DeltaTime();
-        
-        font(L"Hello OpenSiv3D!.").drawAt(Window::Center(), Palette::Black);
-        Circle(Window::Width()/2 + r * cos(5*t), Window::Height()/2 - r * sin(6*t), 10).draw(Palette::Red);
-        
-        /*
+        for(auto i=0; i<N; ++i){
+            Circle(0 + dWidth * i,Window::Height()/2. - 200 * u3[i], 2).draw(Palette::Blue);
+        }
+        //Circle(Window::Width()/2 + r * cos(5*t), Window::Height()/2 - r * sin(6*t), 10).draw(Palette::Red);
         font(L"Hello, Siv3D!🐣").drawAt(Window::Center(), Palette::Black);
-        font(Cursor::Pos()).draw(20, 400, ColorF(0.6));
-        textureCat.resize(80).draw(540, 380);
-        Circle(Cursor::Pos(), 60).draw(ColorF(1, 0, 0, 0.5));
-        */
+
+        for(auto i=1; i<N-1; ++i){
+            u3[i] = 2. * u2[i] - u1[i] + lam * (u2[i+1] - 2. * u2[i] + u2[i-1]);
+        }
+        u3[0] = u3[1];
+        u3[N-1] = u3[N-2];
+        for(auto i=0; i<N; ++i){
+            u1[i] = u2[i];
+        }
+        for(auto i=0; i<N; ++i){
+            u2[i] = u3[i];
+        }
     }
 }
